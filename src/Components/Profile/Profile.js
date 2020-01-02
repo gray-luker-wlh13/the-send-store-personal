c7fff9919427f58372eb58125b2a2e27a541f27b
grayluker05 <graysonluker@gmail.com>
2020-01-01 17:24:00 -0700

    profile displaying user on session

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import './Scss/profile.scss';
 import {connect} from 'react-redux';
 import {logout} from '../../redux/reducers/getConsumerReducer';
 
@@ -7,12 +8,23 @@ const Profile = (props) => {
     let logout = () => {
         axios.post('/api/auth/logout').then(res => {
             props.logout(res.data)
+            props.history.push('/')
         })
     }
 
+    
+    const {consumer} = props.consumer;
+    console.log(props);
     return(
         <div className='profile-container'>
-            <button onClick={logout}>Log Out</button>
+            <div className='profile'>
+                <img src={consumer.profile_img} alt='profile-img'/>
+                <h3>{consumer.username}</h3>
+                <div className='favorite-climb'>
+                    <label>Favorite Climb:</label><div>{consumer.favorite_climb}</div>
+                </div>
+                <button onClick={logout}>Log Out</button>
+            </div>
         </div>
     )
 }
