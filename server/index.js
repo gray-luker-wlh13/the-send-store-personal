693996665af90d667880b26db3b3d16f1bd76b69
grayluker05 <graysonluker@gmail.com>
2020-01-10 10:56:46 -0700

    stuff

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const express = require('express'),
       app = express();
 
 app.use(express.json());
+app.use( express.static( `${__dirname}/../build` ) );
 
 app.use(session({
     resave: false,
